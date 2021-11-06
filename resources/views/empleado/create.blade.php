98e5e1e7f0abed3b271cc16f4bf09cc314cc5eb2
daniela120 <daniela.herrera@ujcv.edu.hn>
2021-11-06 14:02:01 -0600

    u

--- resources/views/empleado/create.blade.php
+++ resources/views/empleado/create.blade.php
@@ -1,4 +1,4 @@
-
+<!-- Modal -->
 <div class="modal animated zoomIn" id="createEmp" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
      aria-hidden="true">
     <div class="modal-dialog" role="document">
