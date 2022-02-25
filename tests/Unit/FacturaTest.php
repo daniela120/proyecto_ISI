e0b0d6119cb6dd5a3e9a0648d9c5913649deafa0
AMSC-Cortes <amoisessc@gmail.com>
2022-02-25 14:29:45 -0600

    Factura listo

--- tests/Unit/FacturaTest.php
+++ tests/Unit/FacturaTest.php
@@ -145,27 +145,55 @@ class FacturaTest extends TestCase
 
 
         $DatosFactura=array(
-            $NumFactura=array('23456789','23456790','23456791'),
-            $FechaFactura=array("2022-01-07","2022-01-09",'2022-01-08'),
-            $Direccion=array('Las Casitas','Los Hidalgos','Kennedy'),
-            $PedidoId=array(2,3,4),
-            $Parametrofac=array(1,1,1),
-            $Empleados=array('Angel','Daniel','Juan'),
-            $Productos=array('Cafe','Sandwich','Smoothie'),
-            $TiposdePago=array(1,2,3),
-            $Clientes=array('Angela','Daniela','Juana'),
-            $Descuento=array(1,2,3),
-            $Isv=array(15,10,5),
+            $NumFactura=array('23456789'),
+            $FechaFactura=array("2022-01-07"),
+            $Direccion=array('Las Casitas'),
+            $PedidoId=array(2),
+            $Parametrofac=array(1),
+            $Empleados=array('Angel'),
+            $Productos=array('Cafe'),
+            $TiposdePago=array(1),
+            $Clientes=array('Angela'),
+            $Descuento=array(1),
+            $Isv=array(15),
 
         
         
         );
 
+        $DetallePedidos=array(
+            $info=['Pedidos_Id'=>'20', 'Id_Producto'=>'02', 'PrecioUnitario'=>12, 'Cantidad'=>5, 'id_descuento'=>0.10, 'id_isv'=>0.15, 'Total'=>'Sindefinir'],
+        ); 
+
+        $Precio=Arr::get($info,'PrecioUnitario');
+        $Cantidad=Arr::get($info,'Cantidad');
+        $Descuento=Arr::get($info,'id_descuento');
+        $Isv=Arr::get($info,'id_isv');
+
+
+      
+                $subtotal=($Precio*$Cantidad );                
+                $sumadescuento=($Precio*$Cantidad)*$Descuento;
+                $sumaimpuesto=($subtotal -$sumadescuento)*$Isv;
+                $Total=($subtotal-$sumadescuento)+$sumaimpuesto;
+
+        $CalculosFactura=array($subtotal,$sumadescuento,$sumaimpuesto,$Total);        
+
+
+
         $datos=$DatosFactura;
         $name=$NumFactura;
+        $name2=$info;
+        $calculo=$subtotal;
+
+        $unionShow=Arr::collapse([$DatosFactura,$DetallePedidos,$CalculosFactura]);
         
         //$this->assertEmpty($array1,'el array no esta vacio');
-        $this->assertContains($name,$datos,"testArray doesn't contains value as value");
+        $this->assertContains($name,$unionShow,"testArray doesn't contains value as value");
+        $this->assertContains($name2,$unionShow,"testArray doesn't contains value as value");
+        $this->assertContains($calculo,$unionShow,"testArray doesn't contains value as value");
+    
+    
     } 
 
 
