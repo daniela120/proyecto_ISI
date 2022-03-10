62887f54c6cea515f4c237c3b0238ccc47bf8b24
Miguel-EdTo <79028297+Miguel-EdTo@users.noreply.github.com>
2022-03-09 22:03:34 -0600

    pie de pagina

--- resources/views/Pedidos/pedidospdf.blade.php
+++ resources/views/Pedidos/pedidospdf.blade.php
@@ -77,7 +77,7 @@
         if ( isset($pdf) ) {
             $pdf->page_script('
                 $font = $fontMetrics->get_font("Arial, Helvetica, sans-serif", "normal");
-                $pdf->text(270, 780, "Pág $PAGE_NUM de $PAGE_COUNT", $font, 10);
+                $pdf->text(270, 790, "Pág $PAGE_NUM de $PAGE_COUNT", $font, 10);
             ');
         }
     </script>   
